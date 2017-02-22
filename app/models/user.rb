b551cbd6d31efe48cd953508fdaa7d68a26db760
Alexis Rabago <alexisraca@hotmail.com>
2017-02-22 00:54:50 -0500

    index from admin/users

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :lockable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  def full_name
+    first_name + last_name
+  end
+
 end
