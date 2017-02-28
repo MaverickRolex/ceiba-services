1a662e5bd67a4b74fe54da582ce9c815929402b5
Alexis Rabago <alexisraca@hotmail.com>
2017-02-28 13:31:19 -0500

    Create, users edit and delete buttons, add user_sign_in validation

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,4 @@
-class Admin::UsersController < ApplicationController
+class Admin::UsersController < Admin::BaseController
 
   def index
     @users = User.all
@@ -17,10 +17,39 @@ class Admin::UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if user_update
+      flash[:notice] = "Usuario actualizado"
+      redirect_to users_path
+    else
+      render :edit
+    end
+  end
+
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy if current_user.admin? && !(current_user == @user)
+    redirect_to users_path
+  end
+
 private
 
   def user_params
      params.require(:user).permit(:first_name, :password, :password_confirmation, :last_name, :email, :admin)
+  end
+
+  def user_update
+    if params[:user][:password].blank? && params[:user][:password_confirmation].blank?
+      @user.update_without_password(user_params)
+    else
+      @user.update_attributes(user_params)
+    end
   end
 
 end
