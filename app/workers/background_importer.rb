a7ef9f4764925956ec2a77b01bdbdbc4de27aba0
Alexis Rabago <alexisraca@hotmail.com>
2017-04-03 23:16:11 -0500

    Include transaction block in to user import process

--- app/workers/background_importer.rb
+++ app/workers/background_importer.rb
@@ -3,9 +3,12 @@ class BackgroundImporter
 
   def perform(user_import_id)
     begin
-      @import = UserImportManager.new(user_import_id)
-      @import.import
+      User.transaction do
+        @import = UserImportManager.new(user_import_id)
+        @import.import
+      end
     rescue Exception => exeption_msgs
+      binding.pry
       @user_import = UserImport.find(user_import_id)
       @user_import.update_attributes(status: UserImport.statuses["failed"])
     end
